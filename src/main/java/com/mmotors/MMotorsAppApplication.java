d59ba1e7afd2ad6b0f278cd290e8bf0b995e4f46
Fabienne Bergès <fabienne_berges@yahoo.fr>
2026-01-14 21:14:16 +0100

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/mmotors/MMotorsAppApplication.java
@@ -0,0 +1,13 @@
+package com.mmotors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MMotorsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MMotorsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mmotors/MMotorsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmotors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MMotorsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
